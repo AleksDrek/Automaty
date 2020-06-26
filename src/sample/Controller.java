55da20c7c989379a771f0cd74003e4b7ae04a58a
kpieta <kamil.pieta@capgemini.com>
2020-06-26 18:33:33 +0200

    lab3

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,8 +1,10 @@
 package sample;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
 import javafx.scene.paint.Color;
 
@@ -15,7 +17,7 @@ public class Controller {
     TextField size;
     @FXML
     TextField iterationsTextField;
-
+    int iterations;
     int arraySize;
     private double cellSize;
     private Cell[][] array;
@@ -26,7 +28,7 @@ public class Controller {
         gc.setFill(Color.WHITE);
         gc.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
 
-        if(array!=null && arraySize!=0){
+        if (array != null && arraySize != 0) {
             for (int i = 0; i < arraySize; i++) {
                 for (int j = 0; j < arraySize; j++) {
                     array[i][j].oldState = 0;
@@ -34,62 +36,94 @@ public class Controller {
                 }
             }
         }
+        putCellInTheMiddle();
     }
 
     public void start() {
-        cleanCanvas();
         initializeData();
+        cleanCanvas();
     }
 
     public void putCellInTheMiddle() {
-        array[arraySize / 2][arraySize / 2].state = 1;
-        array[arraySize / 2][arraySize / 2].oldState = 1;
-        displayData();
+
+        if(array!=null){
+            array[arraySize / 2][arraySize / 2].state = 1;
+            array[arraySize / 2][arraySize / 2].oldState = 1;
+            displayData();
+        }
+
     }
 
-    public void mooreAlgorythm() {
+
+    public void mooreAlgorythmSimulation() {
 
         int interations = Integer.parseInt(iterationsTextField.getText());
 
-        for(int k = 0 ; k<interations; k++) {
+        for (int k = 0; k < interations; k++) {
             for (int i = 0; i < arraySize; i++) {
                 for (int j = 0; j < arraySize; j++) {
 
                     if (array[i][j].oldState == 1) {
-                        //skośny górny lewy
-                        if(i>0 && j>0 && array[i-1][j-1].oldState==0){
-                            array[i-1][j-1].state=1;
-                        }
-                        //skośny górny prawy
-                        if(i>0 && j<arraySize - 1 && array[i-1][j+1].oldState==0){
-                            array[i-1][j+1].state=1;
-                        }
-                        //skośny dolny lewy
-                        if(i<arraySize - 1 && j>0 && array[i+1][j-1].oldState==0){
-                            array[i+1][j-1].state=1;
-                        }
-                        //skośny dolny prawy
-                        if(i<arraySize - 1 && j<arraySize - 1 && array[i+1][j+1].oldState==0){
-                            array[i+1][j+1].state=1;
-                        }
+                        moore(i, j);
+                    }
+                }
+            }
 
-                            // gorny środkowy
-                            if (i > 0 && array[i - 1][j].oldState == 0) {
-                                array[i - 1][j].state = 1;
-                            }
-                            // dolny środkowy
-                            if (i < arraySize - 1 && array[i + 1][j].oldState == 0) {
-                                array[i + 1][j].state = 1;
-                            }
-                            // lewy środkowy
-                            if (j > 0 && array[i][j - 1].oldState == 0) {
-                                array[i][j - 1].state = 1;
-                            }
-                            // prawy środkowy
-                            if (j < arraySize - 1 && array[i][j + 1].oldState == 0) {
-                                array[i][j + 1].state = 1;
-                            }
-                        }
+            for (int i = 0; i < arraySize; i++) {
+                for (int j = 0; j < arraySize; j++) {
+                    array[i][j].oldState = array[i][j].state;
+                }
+            }
+        }
+        displayData();
+    }
+
+
+    private void moore(int i, int j) {
+        //skośny górny lewy
+        if (i > 0 && j > 0 && array[i - 1][j - 1].oldState == 0) {
+            array[i - 1][j - 1].state = 1;
+        }
+        //skośny górny prawy
+        if (i > 0 && j < arraySize - 1 && array[i - 1][j + 1].oldState == 0) {
+            array[i - 1][j + 1].state = 1;
+        }
+        //skośny dolny lewy
+        if (i < arraySize - 1 && j > 0 && array[i + 1][j - 1].oldState == 0) {
+            array[i + 1][j - 1].state = 1;
+        }
+        //skośny dolny prawy
+        if (i < arraySize - 1 && j < arraySize - 1 && array[i + 1][j + 1].oldState == 0) {
+            array[i + 1][j + 1].state = 1;
+        }
+
+        // gorny środkowy
+        if (i > 0 && array[i - 1][j].oldState == 0) {
+            array[i - 1][j].state = 1;
+        }
+        // dolny środkowy
+        if (i < arraySize - 1 && array[i + 1][j].oldState == 0) {
+            array[i + 1][j].state = 1;
+        }
+        // lewy środkowy
+        if (j > 0 && array[i][j - 1].oldState == 0) {
+            array[i][j - 1].state = 1;
+        }
+        // prawy środkowy
+        if (j < arraySize - 1 && array[i][j + 1].oldState == 0) {
+            array[i][j + 1].state = 1;
+        }
+    }
+
+    public void vonNeumanAlgorythm() {
+
+        for (int k = 0; k < iterations; k++) {
+            for (int i = 0; i < arraySize; i++) {
+                for (int j = 0; j < arraySize; j++) {
+
+                    if (array[i][j].oldState == 1) {
+                        vonNeuman(i, j);
+                    }
                 }
             }
 
@@ -101,46 +135,25 @@ public class Controller {
         }
 
         displayData();
-
     }
 
-    public void vonNeumanAlgorythm() {
-
-        int interations = Integer.parseInt(iterationsTextField.getText());
-
-        for(int k = 0 ; k<interations; k++) {
-            for (int i = 0; i < arraySize; i++) {
-                for (int j = 0; j < arraySize; j++) {
-
-                    if (array[i][j].oldState == 1) {
-                        // gorny środkowy
-                        if (i > 0 && array[i - 1][j].oldState == 0) {
-                            array[i - 1][j].state = 1;
-                        }
-                        // dolny środkowy
-                        if (i < arraySize - 1 && array[i + 1][j].oldState == 0) {
-                            array[i + 1][j].state = 1;
-                        }
-                        // lewy środkowy
-                        if (j > 0 && array[i][j - 1].oldState == 0) {
-                            array[i][j - 1].state = 1;
-                        }
-                        // prawy środkowy
-                        if (j < arraySize - 1 && array[i][j + 1].oldState == 0) {
-                            array[i][j + 1].state = 1;
-                        }
-                    }
-                }
-            }
-
-        for (int i = 0; i < arraySize; i++) {
-            for (int j = 0; j < arraySize; j++) {
-                array[i][j].oldState = array[i][j].state;
-            }
+    private void vonNeuman(int i, int j) {
+        // gorny środkowy
+        if (i > 0 && array[i - 1][j].oldState == 0) {
+            array[i - 1][j].state = 1;
         }
+        // dolny środkowy
+        if (i < arraySize - 1 && array[i + 1][j].oldState == 0) {
+            array[i + 1][j].state = 1;
         }
-
-        displayData();
+        // lewy środkowy
+        if (j > 0 && array[i][j - 1].oldState == 0) {
+            array[i][j - 1].state = 1;
+        }
+        // prawy środkowy
+        if (j < arraySize - 1 && array[i][j + 1].oldState == 0) {
+            array[i][j + 1].state = 1;
+        }
     }
 
     private void displayData() {
@@ -154,6 +167,7 @@ public class Controller {
     }
 
     private void initializeData() {
+        iterations = Integer.parseInt(iterationsTextField.getText());
         arraySize = Integer.parseInt(size.getText());
         cellSize = canvas.getHeight() / arraySize;
         array = new Cell[arraySize][arraySize];
@@ -171,4 +185,93 @@ public class Controller {
         cleanCanvas();
     }
 
+    public void hexagonalSymetricSimulation() {
+        for (int k = 0; k < iterations; k++) {
+            for (int i = 0; i < arraySize; i++) {
+                for (int j = 0; j < arraySize; j++) {
+
+                    if (array[i][j].oldState == 1) {
+                        if (i % 2 == 0)
+                            vonNeuman(i, j);
+                        else
+                            moore(i, j);
+                    }
+                }
+            }
+
+            for (int i = 0; i < arraySize; i++) {
+                for (int j = 0; j < arraySize; j++) {
+                    array[i][j].oldState = array[i][j].state;
+                }
+            }
+        }
+
+        displayData();
+
+    }
+
+    public void pentagonalSymetricSimulation() {
+        for (int k = 0; k < iterations; k++) {
+            int counter = 0;
+            for (int i = 0; i < arraySize; i++) {
+                for (int j = 0; j < arraySize; j++) {
+
+                    if (array[i][j].oldState == 1) {
+                        if (counter == 0)
+                            moore(i, j);
+                        else
+                            vonNeuman(i, j);
+                    }
+                }
+                counter++;
+                if (counter > 3) {
+                    counter = 0;
+                }
+            }
+
+            for (int i = 0; i < arraySize; i++) {
+                for (int j = 0; j < arraySize; j++) {
+                    array[i][j].oldState = array[i][j].state;
+                }
+            }
+        }
+
+        displayData();
+    }
+
+    public void heptagonalSymetricSimulation() {
+        for (int k = 0; k < iterations; k++) {
+            int counter = 0;
+            for (int i = 0; i < arraySize; i++) {
+                for (int j = 0; j < arraySize; j++) {
+
+                    if (array[i][j].oldState == 1) {
+                        if (counter == 0)
+                            vonNeuman(i, j);
+                        else
+                            moore(i, j);
+
+                    }
+                }
+                counter++;
+                if (counter > 3) {
+                    counter = 0;
+                }
+            }
+
+            for (int i = 0; i < arraySize; i++) {
+                for (int j = 0; j < arraySize; j++) {
+                    array[i][j].oldState = array[i][j].state;
+                }
+            }
+
+        }
+        displayData();
+    }
+
+    public void pseudoHexagonalSimulation() {
+        Alert alert =
+                new Alert(Alert.AlertType.ERROR, "Kamil nie rozumi polecenia");
+        alert.showAndWait();
+    }
 }
